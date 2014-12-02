22bb3b6aef18ba3e24e806421699d95bf52859e5
Aashish Karki <0133asis@gmail.com>
2014-12-01 23:44:55 -0500

    Dummyclasses working

--- src/abstracts/Run.java
+++ src/abstracts/Run.java
@@ -4,10 +4,14 @@ import abstracts.dummy.DummySuffixTree;
 /* Constructs Suffixtrees out of chapters 
  * Finds LCS using the suffixtree
  * */
+import abstracts.dummy.DummyTextReader;
 
 public class Run {
 	public static void main(String[] args){
-		DummySuffixTree dummySuffixTree = new DummySuffixTree("book1.txt");
-		System.out.println("hey "+ args[0]+" "+args[1]);		
+		DummySuffixTree dummySuffixTree = new DummySuffixTree(args[0]);
+		DummyTextReader dummyTextReader = new DummyTextReader(args[1]);
+		String chapterText = dummyTextReader.chapter(Integer.parseInt(args[2]));
+		String lcs = dummySuffixTree.LCS(chapterText);
+		System.out.println(lcs);		
 	}
 }
