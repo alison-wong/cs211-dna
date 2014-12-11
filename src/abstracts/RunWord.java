b3b3ae5aa73ef176020c13a841a49edb498bc4a2
Kush <kchandra15@amherst.edu>
2014-12-11 03:24:07 -0500

    Created suffix trees of entire words

---
+++ src/abstracts/RunWord.java
@@ -0,0 +1,45 @@
+package abstracts;
+
+/* Constructs Suffixtrees out of chapters 
+ * Finds LCS using the suffixtree
+ * */
+
+import real.BookReaderWord;
+import real.QuadSuffixWord;
+
+public class RunWord {
+	public static void main(String[] args)
+	{
+		BookReaderWord brw = new BookReaderWord("src/book4.txt");
+		long time1 = System.currentTimeMillis();
+		QuadSuffixWord qsw = new QuadSuffixWord(brw.chapterList(21));
+		long time2 = System.currentTimeMillis();
+		System.out.println("It took " + (time2-time1)/1000.0 + " seconds.");
+		
+		for (String s : brw.chapterList(9))
+			System.out.print(s + " ");
+		System.out.println("\n");
+		
+		for (String s : brw.chapterList(21))
+			System.out.print(s + " ");
+		System.out.println("\n");
+		
+		time1 = System.currentTimeMillis();
+		System.out.println(qsw.LCS(brw.chapterList(9)));
+		time2 = System.currentTimeMillis();
+		System.out.println("It took " + (time2-time1)/1000.0 + " seconds.");
+//		System.out.println("Best LCS: " + bestLCS);
+//		int bestLength = 0;
+//		String bestLCS = "";
+//		for (String s2 : bookReader2.book)
+//		{
+//			String LCS = suffixTree.LCS(s2);
+//			System.out.println("LCS: " + LCS);
+//			if (bestLength < LCS.length())
+//			{
+//				bestLength = LCS.length();
+//				bestLCS = LCS;
+//			}
+//		}
+	}
+}
---
+++ src/real/BookReaderWord.java
@@ -0,0 +1,89 @@
+package real;
+
+/*
+ * Reads in a book, converts it to lowercase, and separates it into its chapters.
+ * Chapters in the book need to be marked by "CHAPTER"
+ */
+
+import abstracts.TextReader;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+
+public class BookReaderWord extends TextReader
+{
+	public ArrayList<ArrayList<String>> book;
+	
+	public BookReaderWord(String filename)
+	{
+		super(filename);		//does nothing
+		
+		//read in book - set book to "" if not found
+		Scanner input;
+		book = new ArrayList<ArrayList<String>>();
+		try
+		{
+			input = new Scanner(new File(filename));
+			System.out.println("Loaded book.");
+		}
+		catch (Exception e)
+		{
+			System.out.println("Book \"" + filename + "\" not found.");
+			book.add(new ArrayList<String>());
+			return;
+		}
+		
+		ArrayList<String> currentChapter = new ArrayList<String>();
+		while (input.hasNext())
+		{
+			String next = input.nextLine();
+			if (next.length() > 0)
+			{
+				if (next.startsWith("CHAPTER") && !currentChapter.equals(""))
+				{
+					System.out.println(book.size() + ": " + currentChapter.size());
+					book.add(currentChapter);
+					currentChapter = new ArrayList<String>();
+				}
+				next = next.toLowerCase();
+				StringBuilder word = new StringBuilder();
+				for (int i = 0; i < next.length(); i++)
+				{
+					char c = next.charAt(i);
+					if ((c >= 'a' && c <= 'z') || c == '\'' || c == '-')
+						word.append(c);
+					else
+					{
+						if (word.length() > 0)
+							currentChapter.add(word.toString());
+						word = new StringBuilder();
+					}
+				}
+			}
+		}
+		input.close();
+		
+	}
+
+	public ArrayList<String> chapterList(int chapter)
+	{
+		return book.get(chapter);
+	}
+
+	public String entireBook()
+	{
+		String wholeBook = "";
+		for (int i = 0; i < book.size(); i++)
+		{
+			wholeBook += book.get(i) + " ";
+		}
+		return wholeBook;
+	}
+
+	@Override
+	public String chapter(int chapter)
+	{
+		return "";
+	}	
+}
