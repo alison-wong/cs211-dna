868264f8d0304fb7f1ff34d5e63e6089d90f1f93
Kush <kchandra15@amherst.edu>
2014-12-11 19:43:29 -0500

    Changed BookReaderWord to use BufferedReader
    Also fixed bug where last chapter was cut off sometimes.

--- src/real/BookReaderWord.java
+++ src/real/BookReaderWord.java
@@ -8,7 +8,7 @@ package real;
 import abstracts.TextReader;
 
 import java.util.ArrayList;
-import java.util.Scanner;
+//import java.util.Scanner;
 import java.io.*;
 
 public class BookReaderWord extends TextReader
@@ -20,12 +20,45 @@ public class BookReaderWord extends TextReader
 		super(filename);		//does nothing
 		
 		//read in book - set book to "" if not found
-		Scanner input;
+		BufferedReader input;
 		book = new ArrayList<ArrayList<String>>();
 		try
 		{
-			input = new Scanner(new File(filename));
+			input = new BufferedReader(new FileReader(filename));
 			System.out.println("Loaded book.");
+		
+			ArrayList<String> currentChapter = new ArrayList<String>();
+			while (input.ready())
+			{
+				String next = input.readLine();
+				if (next.length() > 0)
+				{
+					if (next.startsWith("CHAPTER") && !currentChapter.equals(""))
+					{
+						System.out.println(book.size() + ": " + currentChapter.size());
+						book.add(currentChapter);
+						currentChapter = new ArrayList<String>();
+					}
+					next = next.toLowerCase();
+					StringBuilder word = new StringBuilder();
+					for (int i = 0; i < next.length(); i++)
+					{
+						char c = next.charAt(i);
+						if ((c >= 'a' && c <= 'z') || c == '\'' || c == '-')
+							word.append(c);
+						else
+						{
+							if (word.length() > 0)
+								currentChapter.add(word.toString());
+							word = new StringBuilder();
+						}
+					}
+				}
+			}
+			if (currentChapter.size() > 0)
+				book.add(currentChapter);
+			
+			input.close();
 		}
 		catch (Exception e)
 		{
@@ -33,36 +66,6 @@ public class BookReaderWord extends TextReader
 			book.add(new ArrayList<String>());
 			return;
 		}
-		
-		ArrayList<String> currentChapter = new ArrayList<String>();
-		while (input.hasNext())
-		{
-			String next = input.nextLine();
-			if (next.length() > 0)
-			{
-				if (next.startsWith("CHAPTER") && !currentChapter.equals(""))
-				{
-					System.out.println(book.size() + ": " + currentChapter.size());
-					book.add(currentChapter);
-					currentChapter = new ArrayList<String>();
-				}
-				next = next.toLowerCase();
-				StringBuilder word = new StringBuilder();
-				for (int i = 0; i < next.length(); i++)
-				{
-					char c = next.charAt(i);
-					if ((c >= 'a' && c <= 'z') || c == '\'' || c == '-')
-						word.append(c);
-					else
-					{
-						if (word.length() > 0)
-							currentChapter.add(word.toString());
-						word = new StringBuilder();
-					}
-				}
-			}
-		}
-		input.close();
 		
 	}
 
